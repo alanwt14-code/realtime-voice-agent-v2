45dd970a1281ea96e6a620f285595e961215c38f
Alan Thach <alanthach@Alans-MBP.lan>
2026-04-14 08:49:43 -0400

    Add Twilio voice route

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 require('dotenv').config();
 
 const express = require('express');
+const twilio = require('twilio');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Realtime Voice Agent V2 is running');
+});
+
+app.post('/voice', (req, res) => {
+  const twiml = new twilio.twiml.VoiceResponse();
+
+  twiml.say('Hello. Thanks for calling. Our voice assistant is currently being set up. Please leave a message after the tone.');
+
+  twiml.pause({ length: 1 });
+  twiml.say('Goodbye.');
+
+  res.type('text/xml');
+  res.send(twiml.toString());
 });
 
 app.listen(PORT, () => {
